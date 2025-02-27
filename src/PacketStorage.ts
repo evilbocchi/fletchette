7bf9a7c3ce060faafdf0aa380bdf8bbd88e7785c
evil bocchi <treugames@gmail.com>
2025-02-28 02:25:50 +0800

    Add edit mode support

--- src/PacketStorage.ts
+++ src/PacketStorage.ts
@@ -13,13 +13,22 @@ namespace PacketStorage {
     const IS_SERVER = RunService.IsServer();
 
     /**
+     * Whether the current environment is in edit mode.
+     * This is used to determine whether to create new instances.
+     * 
+     * @internal Should not be used outside of this module.
+     */
+    const IS_EDIT = RunService.IsStudio() && !RunService.IsRunning();
+
+    /**
      * PacketStorage folder in ReplicatedStorage.
      */
     export const PACKET_STORAGE = (function () {
-        if (IS_SERVER) {
+        if (IS_SERVER || IS_EDIT) {
             const PacketStorage = new Instance("Folder");
             PacketStorage.Name = "PacketStorage";
-            PacketStorage.Parent = ReplicatedStorage;
+            if (!IS_EDIT)
+                PacketStorage.Parent = ReplicatedStorage;
             return PacketStorage;
         }
         else {
@@ -35,10 +44,11 @@ namespace PacketStorage {
      */
     export const getSignalRemote = (id: string | number, isUnreliable?: boolean) => {
         let remote: RemoteEvent;
-        if (IS_SERVER) {
+        if (IS_SERVER || IS_EDIT) {
             remote = (new Instance(isUnreliable === true ? "UnreliableRemoteEvent" : "RemoteEvent") as BaseRemoteEvent) as RemoteEvent;
             remote.Name = tostring(id);
-            remote.Parent = PACKET_STORAGE;
+            if (!IS_EDIT)
+                remote.Parent = PACKET_STORAGE;
         }
         else {
             remote = PACKET_STORAGE.WaitForChild(id) as RemoteEvent;
@@ -53,10 +63,11 @@ namespace PacketStorage {
      */
     export const getRequestRemote = (id: string | number) => {
         let remote: RemoteFunction;
-        if (IS_SERVER) {
+        if (IS_SERVER || IS_EDIT) {
             remote = new Instance("RemoteFunction");
             remote.Name = tostring(id);
-            remote.Parent = PACKET_STORAGE;
+            if (!IS_EDIT)
+                remote.Parent = PACKET_STORAGE;
         }
         else {
             remote = PACKET_STORAGE.WaitForChild(id) as RemoteFunction;
