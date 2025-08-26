d188de1bad3a7d0a62c5b7c06d0740927064148f
evil bocchi <71329833+evilbocchi@users.noreply.github.com>
2025-08-27 02:51:22 +0800

    Refactor PacketStorage to use a static class
    Converted the PacketStorage namespace to a default-exported static class. Updated internal constants and methods to static properties and methods, improving encapsulation and usage consistency.

--- src/PacketStorage.ts
+++ src/PacketStorage.ts
@@ -4,13 +4,13 @@ import { ReplicatedStorage, RunService } from "@rbxts/services";
  * Utility functions for creating RemoteEvents and RemoteFunctions.
  * Remotes are stored in ReplicatedStorage.PacketStorage.
  */
-namespace PacketStorage {
+export default class PacketStorage {
     
     /**
      * Whether the current environment is server.
      * @internal Should not be used outside of this module.
      */
-    const IS_SERVER = RunService.IsServer();
+    protected static readonly IS_SERVER = RunService.IsServer();
 
     /**
      * Whether the current environment is in edit mode.
@@ -18,16 +18,16 @@ namespace PacketStorage {
      * 
      * @internal Should not be used outside of this module.
      */
-    const IS_EDIT = RunService.IsStudio() && !RunService.IsRunning();
+    protected static readonly IS_EDIT = RunService.IsStudio() && !RunService.IsRunning();
 
     /**
      * PacketStorage folder in ReplicatedStorage.
      */
-    export const PACKET_STORAGE = (function () {
-        if (IS_SERVER || IS_EDIT) {
+    static readonly PACKET_STORAGE = (() => {
+        if (this.IS_SERVER || this.IS_EDIT) {
             const PacketStorage = new Instance("Folder");
             PacketStorage.Name = "PacketStorage";
-            if (!IS_EDIT)
+            if (!this.IS_EDIT)
                 PacketStorage.Parent = ReplicatedStorage;
             return PacketStorage;
         }
@@ -42,38 +42,36 @@ namespace PacketStorage {
      * @param id Unique identifier for the remote
      * @param isUnreliable Whether the remote should be unreliable. Default is false.
      */
-    export const getSignalRemote = (id: string | number, isUnreliable?: boolean) => {
+    static getSignalRemote(id: string | number, isUnreliable?: boolean) {
         let remote: RemoteEvent;
-        if (IS_SERVER || IS_EDIT) {
+        if (this.IS_SERVER || this.IS_EDIT) {
             remote = (new Instance(isUnreliable === true ? "UnreliableRemoteEvent" : "RemoteEvent") as BaseRemoteEvent) as RemoteEvent;
             remote.Name = tostring(id);
-            if (!IS_EDIT)
-                remote.Parent = PACKET_STORAGE;
+            if (!this.IS_EDIT)
+                remote.Parent = this.PACKET_STORAGE;
         }
         else {
-            remote = PACKET_STORAGE.WaitForChild(id) as RemoteEvent;
+            remote = this.PACKET_STORAGE.WaitForChild(id) as RemoteEvent;
         }
         return remote;
-    };
+    }
 
     /**
      * Retrieve a RemoteFunction from PacketStorage. If the RemoteFunction does not exist, it will be created.
      * 
      * @param id Unique identifier for the remote
      */
-    export const getRequestRemote = (id: string | number) => {
+    static getRequestRemote(id: string | number) {
         let remote: RemoteFunction;
-        if (IS_SERVER || IS_EDIT) {
+        if (this.IS_SERVER || this.IS_EDIT) {
             remote = new Instance("RemoteFunction");
             remote.Name = tostring(id);
-            if (!IS_EDIT)
-                remote.Parent = PACKET_STORAGE;
+            if (!this.IS_EDIT)
+                remote.Parent = this.PACKET_STORAGE;
         }
         else {
-            remote = PACKET_STORAGE.WaitForChild(id) as RemoteFunction;
+            remote = this.PACKET_STORAGE.WaitForChild(id) as RemoteFunction;
         }
         return remote;
     };
 }
-
-export default PacketStorage;
